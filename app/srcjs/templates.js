4bfd7c3646cbae1ddd524fefab79ca11508322d0
François Daoust <fd@tidoust.net>
2012-04-26 11:27:48 +0200

    Fixed author name rendering.
    The code was expecting item.author to be a single item whereas it
    is an array in practice.

--- app/srcjs/templates.js
+++ app/srcjs/templates.js
@@ -9,7 +9,7 @@ Joshfire.define(['joshfire/vendor/underscore'], function(_){
   templates.newsTitle = '<h3><%=item.name%></h3>';
   templates.newsImage = '<%if (item.contentURL) {%><div class="image"><img src="<%=item.contentURL%>" alt=""></div><%} else if (item.image && item.image.contentURL){%><div class="image"><img src="<%=item.image.contentURL%>" alt=""></div><%}%>';
   templates.newsContent = '<div class="content"><%=item.description%></div>';
-  templates.newsAuthor = '<%if (item.author){%><div class="author"><span>by </span><%=item.author.name%></div><%}%>';
+  templates.newsAuthor = '<%if (item.author && item.author[0]){%><div class="author"><span>by </span><%=item.author[0].name%></div><%}%>';
   templates.newsCaption = '<div class="caption"><%=item.description%></div>';
   templates.newsHtmlCaption = '<div class="caption"><%if (item.articleBody) {%><%=item.articleBody%><%} else if (item.content) {%><%=item.content%><%} else {%><%=item.description%><%}%></div>';
   templates.news = templates.newsTitle +
